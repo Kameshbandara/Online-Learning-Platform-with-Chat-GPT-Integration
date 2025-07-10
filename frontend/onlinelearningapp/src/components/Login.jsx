7c8843928a9427e6024bc6ff930c0a5a2318f95d
Kameshbandara <kameshb@sltc.edu.lk>
2025-07-10 10:57:19 +0530

    login changes

--- frontend/onlinelearningapp/src/components/Login.jsx
+++ frontend/onlinelearningapp/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import.meta.env.VITE_API_URL
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -24,7 +25,8 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', formData);
+      //const response = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, formData);
       
       // Store token and user info
       localStorage.setItem('token', response.data.token);
